11f5653822873f233b87833ded20deda4513132b
Yury Kozmidi <Kozmidis97@yandex.com>
2021-05-20 17:05:06 +0300

    Исправил замечания

--- script/script.js
+++ script/script.js
@@ -1,11 +1,18 @@
 let editButton = document.querySelector(".profile__redact");
 let popup = document.querySelector(".popup");
 let closeModal = document.querySelector(".popup__close");
-
+let formElement = document.querySelector(".popup__form")
+let nameInput = document.querySelector(".popup__form-input_name")
+let jobInput = document.querySelector(".popup__form-input_job")
+let profileName = document.querySelector(".profile__name")
+let profileAbout = document.querySelector(".profile__about-me")
 
 
 function popupOpen() {
     if (popup.classList.contains('popup_closed')) {
+
+        nameInput.value = profileName.textContent
+        jobInput.value = profileAbout.textContent
         popup.classList.add('popup_opened');
         popup.classList.remove('popup_closed');
     }
@@ -18,19 +25,6 @@ function popupClose() {
     }
 }
 
-editButton.addEventListener('click', popupOpen);
-closeModal.addEventListener('click', popupClose);
-
-
-
-let formElement = document.querySelector(".popup__form")
-let nameInput = document.querySelector(".popup__form-name")
-let jobInput = document.querySelector(".popup__form-aboutMe")
-let profileName = document.querySelector(".profile__name")
-let profileAbout = document.querySelector(".profile__aboutMe")
-
-nameInput.value = profileName.textContent
-jobInput.value = profileAbout.textContent
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
@@ -40,5 +34,6 @@ function formSubmitHandler(evt) {
     popupClose()
 }
 
-
+editButton.addEventListener('click', popupOpen);
+closeModal.addEventListener('click', popupClose);
 formElement.addEventListener('submit', formSubmitHandler);
