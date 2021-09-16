52835e8cac93501ac2a7d38bf2280411849700eb
Yury Kozmidi <Kozmidis97@yandex.com>
2021-09-16 20:09:10 +0300

    добавил метод слушателей событий

--- script/entities/Card.js
+++ script/entities/Card.js
@@ -10,35 +10,19 @@ export class Card {
     }
 
     createCard = () => {
+
+
         this.photoElements = this._getTemplate();
         this._foundElements();
         this.cardImage.src = this._link;
         this.cardName.textContent = this._name;
         this.cardImage.alt = this._name;
 
-        this.cardLike.addEventListener("click", (evt) => {
-            evt.target.classList.toggle("photos__card-like_active");
-        }); //функция лайка/дизлайка карточек
 
-        const popupImage = new Popup("#popup__image");
-        const closeImageModal = document.querySelector(".popup__close_img");
-        closeImageModal.addEventListener("click", popupImage.close);
-
-        this.cardImage.addEventListener("click", (evt) => {
-            evt.target.closest(".popup__image");
-            const popupImg = document.querySelector(".popup__image");
-            const popupImgName = document.querySelector(".popup__image-name");
-            popupImg.src = this._link;
-            popupImg.alt = this._name;
-            popupImgName.textContent = this._name;
-            popupImage.open();
-        });
-
-        this.removeButton.addEventListener("click", (evt) => {
-            evt.target.closest(".photos__card").remove();
-        }); // функция удаления карточки
+        this._setEventListener()
 
         return this.photoElements;
+
     };
 
     _foundElements = () => {
@@ -54,5 +38,32 @@ export class Card {
         const templatePhotos = document.querySelector(this._cardSelector).content;
 
         return templatePhotos.cloneNode(true);
+    }
+
+    _setEventListener() {
+        this.cardLike.addEventListener("click", (evt) => {
+            evt.target.classList.toggle("photos__card-like_active");
+        }); //функция лайка/дизлайка карточек
+
+
+
+        this.removeButton.addEventListener("click", (evt) => {
+            evt.target.closest(".photos__card").remove();
+        });
+        // функция удаления карточки
+        const popupImage = new Popup("#popup__image");
+        const closeImageModal = document.querySelector(".popup__close_img");
+        closeImageModal.addEventListener("click", popupImage.close);
+
+
+        this.cardImage.addEventListener("click", (evt) => {
+            evt.target.closest(".popup__image");
+            const popupImg = document.querySelector(".popup__image");
+            const popupImgName = document.querySelector(".popup__image-name");
+            popupImg.src = this._link;
+            popupImg.alt = this._name;
+            popupImgName.textContent = this._name;
+            popupImage.open();
+        });
     }
 }
